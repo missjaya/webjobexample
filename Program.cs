93567ff53eaf036df363be7cb765eda1f244ce09
missjaya <jayachaudhary888@gmail.com>
2022-12-05 17:51:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace WebJobsSDKSample
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var builder = new HostBuilder();
+            builder.UseEnvironment(EnvironmentName.Development);
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorageQueues();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
+
+
